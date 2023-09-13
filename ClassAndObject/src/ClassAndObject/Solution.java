e0135ac562fe9c811efc1a5aaeb8208e88a89405
sneha0740 <142777983+sneha0740@users.noreply.github.com>
2023-09-13 10:59:44 +0530

    class and object

---
+++ ClassAndObject/src/ClassAndObject/Solution.java
@@ -0,0 +1,11 @@
+package ClassAndObject;
+public class Solution {
+	public static void main(String args[]) {
+//		Student student = new Student();
+//		student.display();
+//		int a = 1;
+		
+		Employee emp = new Employee(1, "Ankit", "Software Development");
+		System.out.println(emp);
+	}
+}
