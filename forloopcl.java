8863816c70ffd5cd48543bca56294cb7a1b61fcc
sneha0740 <142777983+sneha0740@users.noreply.github.com>
2023-10-04 16:43:38 +0530

    forloopcl

---
+++ forloopcl.java
@@ -0,0 +1,17 @@
+package forloop;
+
+public class forloopcl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		        // Using a for loop to print numbers from 1 to 5
+		        for (int i = 1; i <= 5; i++) {
+		            System.out.println(i);
+		        }
+		    }
+		
+
+	}
+
+
