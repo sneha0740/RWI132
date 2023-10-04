375f18139a2e2f6ca1315b55a0fa2ae35770850d
sneha0740 <142777983+sneha0740@users.noreply.github.com>
2023-10-04 16:03:13 +0530

    oopscl

---
+++ oopscl.java
@@ -0,0 +1,27 @@
+package oops1;
+
+
+
+public class oopscl {
+	int employeeid = 2;
+	String employeename= "sneha sharma";
+	int basicsal = 200;
+	int incentive = 300;
+	void countsalarys() {
+		System.out.println(basicsal+incentive);
+	}
+
+}
+
+ class Test  {
+	 
+	 public static void main(String[] args) {
+		
+		 oopscl emp = new oopscl();
+		 
+		
+		System.out.println("objects.employeeid");
+	}
+ }
+
+
