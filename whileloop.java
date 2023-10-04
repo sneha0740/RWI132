abe9985b773989a4366838ae4ff3e521f3573f43
sneha0740 <142777983+sneha0740@users.noreply.github.com>
2023-10-04 16:49:38 +0530

    whileloop

---
+++ whileloop.java
@@ -0,0 +1,20 @@
+package forloop;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i = 1; // initialization
+	        
+	        while (i <= 5) { // condition
+	            System.out.println(i); // code to be executed
+	            i++; // update the loop variable
+	        }
+	        
+	        System.out.println("Loop finished.");
+	    }
+	
+
+	}
+
+
