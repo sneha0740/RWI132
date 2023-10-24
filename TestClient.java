837036e4dfc9e80ce3549ce7121e50a627996bc8
sneha0740 <142777983+sneha0740@users.noreply.github.com>
2023-10-24 20:36:47 +0530

    Add files via upload

---
+++ TestClient.java
@@ -0,0 +1,14 @@
+package com.sathya.client;
+
+import com.sathya.operations.Operations;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+         Operations operations=new Operations();
+         operations.add();
+         Operations.mul();
+         
+	}
+
+}
